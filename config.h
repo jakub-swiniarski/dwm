f959176a5bab1a7fe1d3fd7e8967eb88ba122319
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2023-08-30 19:51:15 +0200

    removed cyan color definition

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]		= "#005577";
+//static const char col_cyan[]		= "#005577";
 static const char col_green[]       = "#3C9D00";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
