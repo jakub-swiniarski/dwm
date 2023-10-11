7bcca4e927b122d5923f45b3d51968a55565b7dd
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2023-10-11 17:39:49 +0200

    switched to alacritty

--- config.h
+++ config.h
@@ -58,7 +58,7 @@ static const char *mutevol[]    = { "/usr/bin/wpctl",   "set-mute",   "@DEFAULT_
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_white, "-sb", col_green, "-sf", col_white, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
