2dc7681e5d2fffb2021af8796d26c9e161f78475
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2023-11-04 20:43:11 +0100

    do not draw the current layout

--- dwm.c
+++ dwm.c
@@ -705,7 +705,7 @@ drawbar(Monitor *m)
 	}
 	w = TEXTW(m->ltsymbol);
 	drw_setscheme(drw, scheme[SchemeNorm]);
-	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
+	//x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
 	if ((w = m->ww - tw - x) > bh) {
 		if (m->sel) {
