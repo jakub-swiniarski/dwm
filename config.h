9acf4d1ef2fa6aaa318f196448f78d176729a50c
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-02-03 21:49:42 +0100

    update config.h

--- config.h
+++ config.h
@@ -12,8 +12,8 @@ static const char *colors[][3]      = {
 static const char *tags[] = { "1", "2", "3", "4", "5"};
 
 /* layout(s) */
-static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
+static const float mfact = 0.5; /* factor of master area size [0.05..0.95] */
+static const int nmaster = 1;    /* number of clients in master area */
 
 /* key definitions */
 #define ALT Mod1Mask
