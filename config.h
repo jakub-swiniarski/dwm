55be55db47aaea0e0dc0f37212bbb5c643f1f4e8
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-03-17 17:57:01 +0100

    capitalize all constants

--- config.h
+++ config.h
@@ -14,9 +14,9 @@ static const int NMASTER = 1;
 #define ALT Mod1Mask
 #define SUPER Mod4Mask
 #define TAGKEYS(KEY,TAG) \
-	{ SUPER,             KEY, view,       { .ui = 1 << TAG} }, \
-	{ SUPER|ControlMask, KEY, toggleview, { .ui = 1 << TAG} }, \
-	{ SUPER|ShiftMask,   KEY, tag,        { .ui = 1 << TAG} },
+	{ SUPER,             KEY, view,       { .UI = 1 << TAG} }, \
+	{ SUPER|ControlMask, KEY, toggleview, { .UI = 1 << TAG} }, \
+	{ SUPER|ShiftMask,   KEY, tag,        { .UI = 1 << TAG} },
 
 static const char *UPVOL[]   = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+10%",   NULL };
 static const char *DOWNVOL[] = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "-10%",   NULL };
@@ -27,21 +27,21 @@ static const char *DMENUCMD[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const char *TERMCMD[]  = { "st", NULL };
 
 static const Key KEYS[] = {
-	{ SUPER,           XK_r,   spawn,      { .v = DMENUCMD } },
-	{ SUPER,           XK_t,   spawn,      { .v = TERMCMD } },
-	{ SUPER,           XK_j,   focusstack, { .i = +1 } },
-	{ SUPER,           XK_k,   focusstack, { .i = -1 } },
+	{ SUPER,           XK_r,   spawn,      { .V = DMENUCMD } },
+	{ SUPER,           XK_t,   spawn,      { .V = TERMCMD } },
+	{ SUPER,           XK_j,   focusstack, { .I = +1 } },
+	{ SUPER,           XK_k,   focusstack, { .I = -1 } },
 	{ SUPER,           XK_z,   zoom,       { 0 } },
 	{ SUPER,		   XK_c,   killclient, { 0 } },
-	{ SUPER,           XK_h,   focusmon,   { .i = -1 } },
-	{ SUPER,           XK_l,   focusmon,   { .i = +1 } },
-	{ SUPER|ShiftMask, XK_h,   tagmon,     { .i = -1 } },
-	{ SUPER|ShiftMask, XK_l,   tagmon,     { .i = +1 } },
+	{ SUPER,           XK_h,   focusmon,   { .I = -1 } },
+	{ SUPER,           XK_l,   focusmon,   { .I = +1 } },
+	{ SUPER|ShiftMask, XK_h,   tagmon,     { .I = -1 } },
+	{ SUPER|ShiftMask, XK_l,   tagmon,     { .I = +1 } },
 	{ SUPER|ShiftMask, XK_q,   quit,       { 0 } },
 	{ SUPER,           XK_b,   togglebar,  { 0 } },
-	{ ALT,             XK_F3,  spawn,      {.v = UPVOL   } },
-	{ ALT,             XK_F2,  spawn,      {.v = DOWNVOL } },
-	{ ALT,             XK_F1,  spawn,      {.v = MUTEVOL } },
+	{ ALT,             XK_F3,  spawn,      {.V = UPVOL   } },
+	{ ALT,             XK_F2,  spawn,      {.V = DOWNVOL } },
+	{ ALT,             XK_F1,  spawn,      {.V = MUTEVOL } },
 	TAGKEYS(XK_1, 0)
 	TAGKEYS(XK_2, 1)
 	TAGKEYS(XK_3, 2)
